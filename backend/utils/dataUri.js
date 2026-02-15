a05a22179b67d0befdece5da1f9f42d87ef21aa2
Vishnu Sahu <vs2917239@gmail.com>
2026-02-16 00:13:51 +0530

    fix: correct datauri import and dependency

--- backend/utils/dataUri.js
+++ backend/utils/dataUri.js
@@ -1,11 +1,11 @@
-import dataUriParser from "datauri/parser.js"
-import path from "path"
+import DatauriParser from "datauri/parser.js";
+import path from "path";
 
-const parser = new dataUriParser()
+const parser = new DatauriParser();
 
 const getDataUri = (file) => {
-    const extName = path.extname(file.originalname).toString();
-    return parser.format(extName, file.buffer).content;
-}
+  const extName = path.extname(file.originalname).toString();
+  return parser.format(extName, file.buffer);
+};
 
 export default getDataUri;
