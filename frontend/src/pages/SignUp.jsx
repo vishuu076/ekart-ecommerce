e5f50b605952b9ec7455c7b36e0f82ad41af780b
Vishnu Sahu <vs2917239@gmail.com>
2026-02-15 18:42:49 +0530

    fix: replace localhost with VITE_URL

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Eye , EyeOff, Loader2 } from "lucide-react";
+import { Eye, EyeOff, Loader2 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "sonner";
@@ -40,18 +40,23 @@ const SignUp = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(formData);
-        try{
+        try {
             setLoading(true);
-            const res = await axios.post("http://localhost:8000/api/auth/register", formData, {
-                headers: {
-                    'Content-Type': 'application/json'
+            axios.post(
+                `${import.meta.env.VITE_URL}/api/auth/register`,
+                formData,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
                 }
-            });
-            if(res.data.success){
+            );
+
+            if (res.data.success) {
                 navigate("/verify");
                 toast.success(res.data.message)
             }
-        }catch(err){
+        } catch (err) {
             console.log(err);
             toast.error(err.response.data.message);
         }
@@ -68,72 +73,72 @@ const SignUp = () => {
                 </CardHeader>
 
                 <CardContent>
-                        <div className="flex flex-col gap-3">
-                            <div className="grid grid-cols-2 gap-4">
-                                <div className="grid gap-2">
-                                    <Label htmlFor="firstName">First Name</Label>
-                                    <Input
-                                        id="firstName"
-                                        name="firstName"
-                                        type="text"
-                                        placeholder="John"
-                                        value={formData.firstName}
-                                        onChange={handleChange}
-                                        required
-                                    />
-                                </div>
-                                <div className="grid gap-2">
-                                    <Label htmlFor="lastName">Last Name</Label>
-                                    <Input
-                                        id="lastName"
-                                        name="lastName"
-                                        type="text"
-                                        placeholder="Doe"
-                                        value={formData.lastName}
-                                        onChange={handleChange}
-                                        required
-                                    />
-                                </div>
-                            </div>
-                            <Label htmlFor="email">Email</Label>
-                            <Input
-                                id="email"
-                                name="email"
-                                type="email"
-                                placeholder="m@example.com"
-                                value={formData.email}
-                                onChange={handleChange}
-                                required
-                            />
-
+                    <div className="flex flex-col gap-3">
+                        <div className="grid grid-cols-2 gap-4">
                             <div className="grid gap-2">
-                                <div className="flex items-center">
-                                    <Label htmlFor="password">Password</Label>
-                                </div>
-                                <div className="relative">
-                                    <Input id="password" 
-                                    name="password" 
-                                    placeholder= "Create a strong password"
+                                <Label htmlFor="firstName">First Name</Label>
+                                <Input
+                                    id="firstName"
+                                    name="firstName"
+                                    type="text"
+                                    placeholder="John"
+                                    value={formData.firstName}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label htmlFor="lastName">Last Name</Label>
+                                <Input
+                                    id="lastName"
+                                    name="lastName"
+                                    type="text"
+                                    placeholder="Doe"
+                                    value={formData.lastName}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <Label htmlFor="email">Email</Label>
+                        <Input
+                            id="email"
+                            name="email"
+                            type="email"
+                            placeholder="m@example.com"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                            </div>
+                            <div className="relative">
+                                <Input id="password"
+                                    name="password"
+                                    placeholder="Create a strong password"
                                     type={showPassword ? "text" : "password"}
                                     value={formData.password}
                                     onChange={handleChange}
-                                     required />
-                                     {
-                                        showPassword ? <EyeOff className="w-5 h-5 text-gray-700 absolute right-5 bottom-2" onClick={() => setShowPassword(false)} /> :
-                                         <Eye className="w-5 h-5 text-gray-700 absolute right-2 top-2" onClick={() => setShowPassword(true)} />
-                                     }
-                                </div>
+                                    required />
+                                {
+                                    showPassword ? <EyeOff className="w-5 h-5 text-gray-700 absolute right-5 bottom-2" onClick={() => setShowPassword(false)} /> :
+                                        <Eye className="w-5 h-5 text-gray-700 absolute right-2 top-2" onClick={() => setShowPassword(true)} />
+                                }
                             </div>
                         </div>
+                    </div>
                 </CardContent>
 
                 <CardFooter className="flex-col gap-2">
-                    <Button  onClick={handleSubmit} className="w-full cursor-pointer bg-pink-600 hover:bg-pink-500" >
-                       {loading?<><Loader2 className="h-4 w-4 animate-spin mr-2"/>Please wait...</> : "Sign Up"}
+                    <Button onClick={handleSubmit} className="w-full cursor-pointer bg-pink-600 hover:bg-pink-500" >
+                        {loading ? <><Loader2 className="h-4 w-4 animate-spin mr-2" />Please wait...</> : "Sign Up"}
                     </Button>
-                   <p className="text-gray-700 sm">
-                    Already have an account? <a href="/login" className="hover:underline cursor-pointer text-pink-800">Login</a>
-                   </p>
+                    <p className="text-gray-700 sm">
+                        Already have an account? <a href="/login" className="hover:underline cursor-pointer text-pink-800">Login</a>
+                    </p>
                 </CardFooter>
             </Card>
         </div>
