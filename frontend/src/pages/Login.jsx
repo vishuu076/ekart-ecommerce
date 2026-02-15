17cc9667aa54d684a68f1ca4b9a137d5b9dcdf9c
Vishnu Sahu <vs2917239@gmail.com>
2026-02-16 01:09:31 +0530

    fix: hardcoded backend url and fixed res error

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -42,8 +42,7 @@ const Login = () => {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await axios.post(`${import.meta.env.VITE_URL}/api/auth/login`, 
-        formData, {
+      const res = await axios.post("https://ekart-ecommerce-i9h9.onrender.com/api/auth/login", formData, {
         headers: {
           'Content-Type': 'application/json'
         }
@@ -56,98 +55,98 @@ const Login = () => {
         toast.success(res.data.message)
       }
     } catch (err) {
-      console.log(err);
-      toast.error(err.response.data.message);
+      console.error(err);
+      // err.response check karo, res nahi!
+      const msg = err.response?.data?.message || "Server Error";
+      toast.error(msg);
     }
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-pink-100">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              Enter given details to login to your account
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <div className="flex flex-col gap-3">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="m@example.com"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <div className="relative">
+                  <Input
+                    id="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    type={showPassword ? "text" : "password"}
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                  />
+                  {showPassword ? (
+                    <EyeOff
+                      className="w-5 h-5 text-gray-700 absolute right-5 bottom-2 cursor-pointer"
+                      onClick={() => setShowPassword(false)}
+                    />
+                  ) : (
+                    <Eye
+                      className="w-5 h-5 text-gray-700 absolute right-2 top-2 cursor-pointer"
+                      onClick={() => setShowPassword(true)}
+                    />
+                  )}
+                </div>
+                <p
+                  onClick={() => navigate("/forgot-password")}
+                  className="text-sm text-pink-700 cursor-pointer hover:underline text-center"
+                >
+                  Forgot password?
+                </p>
+
+              </div>
+            </div>
+          </CardContent>
+
+          <CardFooter className="flex-col gap-2">
+            <Button
+              onClick={handleSubmit}
+              className="w-full cursor-pointer bg-pink-600 hover:bg-pink-500"
+            >
+              {loading ? (
+                <>
+                  <Loader2 className="h-4 w-4 animate-spin mr-2" />
+                  Please wait...
+                </>
+              ) : (
+                "Login"
+              )}
+            </Button>
+
+            <p className="text-gray-700 text-sm">
+              Don't have an account?{" "}
+              <span
+                onClick={() => navigate("/signup")}
+                className="hover:underline cursor-pointer text-pink-800"
+              >
+                Signup
+              </span>
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
+    );
+
   }
 
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-pink-100">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
-          <CardDescription>
-            Enter given details to login to your account
-          </CardDescription>
-        </CardHeader>
-
-        <CardContent>
-          <div className="flex flex-col gap-3">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="m@example.com"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <div className="relative">
-                <Input
-                  id="password"
-                  name="password"
-                  placeholder="Enter your password"
-                  type={showPassword ? "text" : "password"}
-                  value={formData.password}
-                  onChange={handleChange}
-                  required
-                />
-                {showPassword ? (
-                  <EyeOff
-                    className="w-5 h-5 text-gray-700 absolute right-5 bottom-2 cursor-pointer"
-                    onClick={() => setShowPassword(false)}
-                  />
-                ) : (
-                  <Eye
-                    className="w-5 h-5 text-gray-700 absolute right-2 top-2 cursor-pointer"
-                    onClick={() => setShowPassword(true)}
-                  />
-                )}
-              </div>
-              <p
-                onClick={() => navigate("/forgot-password")}
-                className="text-sm text-pink-700 cursor-pointer hover:underline text-center"
-              >
-                Forgot password?
-              </p>
-
-            </div>
-          </div>
-        </CardContent>
-
-        <CardFooter className="flex-col gap-2">
-          <Button
-            onClick={handleSubmit}
-            className="w-full cursor-pointer bg-pink-600 hover:bg-pink-500"
-          >
-            {loading ? (
-              <>
-                <Loader2 className="h-4 w-4 animate-spin mr-2" />
-                Please wait...
-              </>
-            ) : (
-              "Login"
-            )}
-          </Button>
-
-          <p className="text-gray-700 text-sm">
-            Don't have an account?{" "}
-            <span
-              onClick={() => navigate("/signup")}
-              className="hover:underline cursor-pointer text-pink-800"
-            >
-              Signup
-            </span>
-          </p>
-        </CardFooter>
-      </Card>
-    </div>
-  );
-
-}
-
-export default Login;
+  export default Login;
